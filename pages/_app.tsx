8a45d129d842afb09ec5ec38c077997218316e01
Maximilian Skowron <derskoffi@gmail.com>
2023-04-14 14:57:23 +0200

    feat(*): add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import 'skowron/styles/globals.css'
-import type { AppProps } from 'next/app'
-import BaseLayout from 'skowron/components/BaseLayout';
+import "skowron/styles/globals.css";
+import type { AppProps } from "next/app";
+import BaseLayout from "skowron/components/BaseLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <BaseLayout>
-    <Component {...pageProps} />
-  </BaseLayout>
+    <>
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+      <Analytics />
+    </>
   );
 }
